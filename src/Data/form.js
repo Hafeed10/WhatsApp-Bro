1ac339d7aa05325bfd47807a3a4f2258f65f1ce0
Hafeed10 <hafeex@gamil.com>
2024-05-29 06:37:40 +0530

    port 2.5

---
+++ src/Data/form.js
@@ -0,0 +1,39 @@
+import { images } from "./imagePage";
+export const form = [
+    {
+      id: 1,
+      image: images[10],
+      name: 'Hafeex',
+      message: 'Good morning'
+    },
+    {
+      id: 2,
+      image: images[11],
+      name: 'Muhammed',
+      message: 'Hello there'
+    },
+    {
+      id: 3,
+      image: images[12],
+      name: 'Thasniya',
+      message: 'How are you?'
+    },
+    {
+      id: 4,
+      image: images[13],
+      name: 'Vahab',
+      message: 'What’s up?'
+    },
+    {
+      id: 5,
+      image: images[14],
+      name: 'Rafeeq',
+      message: 'Good evening'
+    },
+    // {
+    //   id: 6,
+    //   image: images[14],
+    //   name: 'Abdul Kalam',
+    //   message: 'Good night'
+    // }
+  ];
---
+++ src/Data/imagePage.js
@@ -0,0 +1,53 @@
+import logo from '../../src/Assets/chat (1).svg';
+import logo1 from '../../src/Assets/chats.svg';
+import logo2 from '../../src/Assets/Property 1=video-camera.svg';
+import logo3 from '../../src/Assets/Property 1=phone.svg';
+import logo4 from '../../src/Assets/Property 1=users.svg';
+import logo5 from '../../src/Assets/Property 1=setting (1).svg';
+import logo6 from '../../src/Assets/Profile (1).jpg';
+import logo7 from '../../src/Assets/icons.svg';
+import logo8 from '../../src/Assets/Profile (7).jpg';
+import logo9 from '../../src/Assets/White Option.svg';
+import logo10 from '../../src/Assets/Profile (11).jpg';
+import logo11 from '../../src/Assets/Profile (12).jpg';
+import logo12 from '../../src/Assets/Profile (13).jpg';
+import logo13 from '../../src/Assets/Profile (15).jpg';
+import logo14 from '../../src/Assets/Profile (8).jpg';
+import logo15 from '../../src/Assets/grey dots.svg';
+import logo16 from '../../src/Assets/attachment 2.svg';
+import logo17 from '../../src/Assets/white send.svg';
+import logo18 from '../../src/Assets/white send.svg';
+import logo19 from '../../src/Assets/Vector.svg';
+import logo20 from '../../src/Assets/Option.svg';
+import logo21 from '../../src/Assets/image_processing20220225-29217-hv4dvv.jpg';
+import logo22 from '../../src/Assets/image_processing20220305-7813-19y4c1j.jpg';
+import logo23 from '../../src/Assets/Component 96.svg';
+import logo24 from '../../src/Assets/Component 96.svg';
+
+export const images = [
+    logo,
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo6,
+    logo7,
+    logo8,
+    logo9,
+    logo10,
+    logo11,
+    logo12,
+    logo13,
+    logo14,
+    logo15,
+    logo16,
+    logo17,
+    logo18,
+    logo19,
+    logo20,
+    logo21,
+    logo22,
+    logo23,
+    logo24,
+];
